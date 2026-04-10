98c538809082a6832ca08d84194b038d0b14e897
Ryan Chan <ryan@rcchan.com>
2026-04-10 23:19:27 +0900

    Allow ChannelMessage to surface errors

--- core_lib/src/channel.rs
+++ core_lib/src/channel.rs
@@ -27,6 +27,27 @@ pub enum TransferType {
     Outbound,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize, TS)]
+#[ts(export)]
+#[serde(tag = "type", content = "data")]
+pub enum ChannelError {
+    // Add other specific variants here as needed
+    Generic { message: String, debug: String },
+}
+
+pub trait ToChannelError {
+    fn to_channel_error(&self) -> ChannelError;
+}
+
+impl<E: std::fmt::Display + std::fmt::Debug> ToChannelError for E {
+    fn to_channel_error(&self) -> ChannelError {
+        ChannelError::Generic {
+            message: self.to_string(),
+            debug: format!("{:?}", self),
+        }
+    }
+}
+
 #[derive(Debug, Clone, Default, Deserialize, Serialize, TS)]
 #[ts(export)]
 pub struct ChannelMessage {
@@ -40,4 +61,15 @@ pub struct ChannelMessage {
     pub rtype: Option<TransferType>,
     pub state: Option<State>,
     pub meta: Option<TransferMetadata>,
+    pub error: Option<ChannelError>,
+}
+
+impl ChannelMessage {
+    pub fn set_error<E: ToChannelError>(&mut self, err: E) {
+        self.error = Some(err.to_channel_error());
+    }
+    pub fn with_error<E: ToChannelError>(mut self, err: E) -> Self {
+        self.set_error(err);
+        self
+    }
 }
